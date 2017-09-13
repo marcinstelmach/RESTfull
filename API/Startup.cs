7ac986d2266b4b006bbb7e3e2a5ffd88d26c4265
Marcin Stelmah <marcinxd4@gmail.com>
2017-09-13 12:40:03 +0200

    Initial Commit

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,7 +32,7 @@ namespace API
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services) 
         {
             services.AddDbContext<LibraryContext>(
                 option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
